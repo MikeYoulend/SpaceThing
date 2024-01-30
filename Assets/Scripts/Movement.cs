e1e921eea3828667744458e1eb1268267ed09e9f
MikeYoulend <mikeyoulend@gmail.com>
2024-01-30 22:15:19 +0100

    Rocket Project Unity

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{   
+    [SerializeField] float rotationForce = 100f;
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(UnityEngine.Vector3.up * mainThrust * Time.deltaTime); //is a short for (0, 1, 0)
+        }
+    }
+
+    void ProcessRotation()
+    {
+         if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationForce);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationForce);  //UnityEngine.Vector3.back other way to write 
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;  //freezing rotation so we can manually rotate
+        transform.Rotate(UnityEngine.Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; //unfreezing so the physics system can take over
+    }
+}
