ea5f43b90e9f58f1194b5694800adda090562b57
MikeYoulend <mikeyoulend@gmail.com>
2024-02-01 14:49:55 +0100

    CollisionHandler

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        switch (other.gameObject.tag)
+        {
+            case ""
+        }
+    }
+}
