e0c8cb03176fdf7f1e7de1a30422f6f575805977
MikeYoulend <mikeyoulend@gmail.com>
2024-02-04 16:48:41 +0100

    Mathf.Sin() tau

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    UnityEngine.Vector3 startingPosition;
-    [SerializeField] UnityEngine.Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor; //Range delimita il movivento tra 0 e 1
+    UnityEngine.Vector3 startingPosition; //indica la posizione in cui si trova l'oggetto
+    [SerializeField] UnityEngine.Vector3 movementVector; //aumenta esponezialmente il range di movimento
+    float movementFactor; //Range delimita il movivento tra 0 e 1
+    [SerializeField] float period = 2f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = transform.position;
-        Debug.Log(startingPosition);
+        startingPosition = transform.position; //dichiaro dove si trova l'oggetto   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float cycles = Time.time / period; //Ciclo di vita, Es. Per arrivare da -2 a più 2, QUANTO TEMPO VOGLIAMO?
+
+        const float tau = Mathf.PI * 2; //Definiamo un tau che sono due Pi greco
+        float rawSinWave = Mathf.Sin(cycles * tau); //movimento da -Numero a +Numero
+
+        // La variabile rawSinWave contiene il valore del seno che varia tra -1 e 1.
+        // Questa riga trasforma quel valore da [-1, 1] a [0, 1] per essere usato come fattore di movimento.
+        movementFactor = (rawSinWave + 1f) / 2f; //+1f serve per farlo partire da 0 invece che -1
+
+       UnityEngine.Vector3 offset = movementVector * movementFactor; //dichiaro quanto si dovrà muovere 
+       transform.position = startingPosition + offset; //dichiaro fino a dove arriverà l'oggetto
     }
 }
